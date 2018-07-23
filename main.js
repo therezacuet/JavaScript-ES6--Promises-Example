a1f394d0f0e4f4340e334e2504bacb62fa7fb4a8
Rezaul Islam <rejaulcuet88@gmail.com>
2018-07-23 17:05:16 +0600

    Update main.js

--- main.js
+++ main.js
@@ -39,16 +39,60 @@ function getData(method, url){
   });
 }
 
-getData('GET', 'https://raw.githubusercontent.com/therezacuet/Project-Report/master/jsndata.json').then(function(data){
+getData('GET', 'https://jsonplaceholder.typicode.com/users').then(function(data){
   let users = JSON.parse(data);
   let output = '';
   for(let user of users){
     output += `
-      <li>
-        
-        <h3>Email: ${user.email}</h3>
-        <p>Phone: ${user.phone}</p>
-      </li>
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                    <div class="mainflip">
+                        <div class="frontside">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <p><img class=" img-fluid" src="https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_01.png" alt="card image"></p>
+                                    <h4 class="card-title">${user.name}</h4>
+                                    <h5>${user.company.bs}</h5>
+                                    <p class="card-text">${user.company.name}</p>
+                                    <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="backside">
+                            <div class="card">
+                                <div class="card-body text-center mt-4">
+                                    <h4 class="card-title">${user.name}</h4>
+                                    <p class="card-text">${user.address.street},${user.address.suite},${user.address.city},${user.address.zipcode}</p>
+                                    <p class="card-text">${user.email}<br>${user.phone}<br>${user.website}</p>
+      
+                                    <ul class="list-inline">
+                                        <li class="list-inline-item">
+                                            <a class="social-icon text-xs-center" target="_blank" href="#">
+                                                <i class="fa fa-facebook"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a class="social-icon text-xs-center" target="_blank" href="#">
+                                                <i class="fa fa-twitter"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a class="social-icon text-xs-center" target="_blank" href="#">
+                                                <i class="fa fa-skype"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a class="social-icon text-xs-center" target="_blank" href="#">
+                                                <i class="fa fa-google"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
     `;
   }
 
